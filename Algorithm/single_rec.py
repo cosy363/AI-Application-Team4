cde2e3b235185011fad6e8808a6ffb8e2f083b5e
Jin Kwon Kim <cosy363@naver.com>
2022-05-25 11:31:54 +0900

    Update single_rec.py

--- Algorithm/single_rec.py
+++ Algorithm/single_rec.py
@@ -1,15 +1,17 @@
 # 어울림 계수는 1점 만점으로
 
 
-def single_rec(database,root_furniture,base_furniture1,base_furniture2):
+def single_rec(database,preference,root_furniture,base_furniture1,base_furniture2,base_furniture3):
     abs = 0
     rel = 0
     
     #id를 기준으로 생각하자..
+
     
 
     #절대적인 가구 추천 계수
-
+    # rating 
+    # number of reviews는 linear하게 적용하자
 
     score = abs + rel
 
