d1de3ccbe3c1fe843bbfbbacdc14fa92f622783c
Jin Kwon Kim <cosy363@naver.com>
2022-06-06 03:29:06 +0900

    So much develop
    -Single Algorithm Completion
    - Imshow for showcase
    - Store combinations to Mysql DB

--- Algorithm/main.py
+++ Algorithm/main.py
@@ -1,25 +1,46 @@
-from cmath import sin
+from concurrent.futures import thread
 import json
 import pymysql
-from single_rec import single_rec,single_comb
-
+import random
+from single_rec import single_comb
+from imshow import imshow
+import numpy as np
 
 #1.REST API 형식의 유저 요청 들어옴
 #json structure
 json_string='''{
     "id": 1,
-    "username": "Bret",
-    "furniture preference": [1,2,5,3],
-    "color preference": [1,3,6,4]
+    "username": "Jinkwon",
+    "furniture preference": [2,9,6,8],
+    "color preference": [1,4,5]
 }'''
 
 json_object = json.loads(json_string)
 
+manual_input = True
 
 #inputs: user color, furniture combination
 user_preference = {}
 user_preference['user_color'] = json_object['color preference']
-user_preference['furniture_combination'] = json_object['furniture preference'].sorted
+user_preference['furniture_combination'] = sorted(json_object['furniture preference'])
+
+######TESTCASE FOR MANUAL COMBINATION ASSESSMENT ONLY##############################
+if manual_input == False:
+    color_rand, furniture_rand = [1,2,3,4,5,6], [1,2,3,4,5,6,7,8,9]
+
+    color_rand = random.sample(color_rand,4)
+    furniture_rand = random.sample(furniture_rand,4)
+
+    user_preference['user_color'] = sorted(color_rand)
+    user_preference['furniture_combination'] = sorted(furniture_rand)
+##################################################################################
+print("FURNITURE PREFERENCE: ",str(user_preference['furniture_combination']))
+print("COLOR PREFERENCE: ",str(user_preference['user_color']))
+
+
+# Convert User Color number for array indexing
+for i,k in enumerate(user_preference['user_color']):
+    user_preference['user_color'][i] -= 1
 
 #2.rearrange furniture combination by 우선도
 
@@ -33,30 +54,60 @@ prime_list,second_list,third_list,final_list = [],[],[],[]
 prime_list = single_comb(user_preference,1)
 # output: 10 Ps
 
-##3.2: second가구 
-#[p1,p2p3...]
+##3.2: second가구
+# single_rec.py for each 10 Ps = 80 PXS
 second_list = single_comb(user_preference,2,prime_list)
 
+##3.3: third가구
+# single_rec.py for each 80 PXS = 400 PXSXT
 third_list = single_comb(user_preference,3,second_list)
 
+#3.4: final가구
+# single_rec.py for 400 PXSXT = 800 PXSXTXF
 final_list = single_comb(user_preference,4,third_list)
 
+print("Combination Generated!")
 
-# import third가구 category dB from mysql
-# single_rec.py for each 10 Ps
-# output: 80 PxS    
-#3.3: third가구
-# import third가구 category dB from mysql
-# single_rec.py for each 80 PXS
-# output: 400 PXSXT
-#3.4: second가구
-# import prime가구 category dB from mysql
-# single_rec.py for 400 PXSXT
-# output: 800 PXSXTXF
-# output: PSTFs[product_number], sum of single_scores of combination
-#  
+#3.5: sum of combination
+
+for i,a in enumerate(final_list):
+    for j,b in enumerate(final_list[i]):
+        for k,c in enumerate(final_list[i][j]):
+            for l,d in enumerate(final_list[i][j][k]):
+                sum = 0.
+                price_sum = 0
+                for m,e in enumerate(final_list[i][j][k][l]):
+                    sum += final_list[i][j][k][l][m]['single score']
+                    price_sum += final_list[i][j][k][l][m]['Price']
+                final_list[i][j][k][l].append(sum)
+                final_list[i][j][k][l].append(price_sum)
+            
+                pri = final_list[i][j][k][l][0]['New_ID']
+                sec = final_list[i][j][k][l][1]['New_ID']
+                thr = final_list[i][j][k][l][2]['New_ID']
+                fin = final_list[i][j][k][l][3]['New_ID']
+                sql = "INSERT INTO furniture_DB.combination_log (product_id1, product_id2, product_id3, product_id4, single_score_sum) VALUES (%d, %d, %d, %d, %f)"
+                
+                #3.6 Send to mysql DB
+                conn2, cur2 = None, None
+                conn2 = pymysql.connect(host='127.0.0.1',user='root',password='kimjin12',db='furniture_DB',charset='utf8')
+
+                
+
+                with conn2:
+                    with conn2.cursor() as cur2:
+                        cur2.execute(sql % (pri,sec,thr,fin,sum))
+                        conn2.commit()
+
+print("Price Sum: "+str(final_list[0][0][0][0][5])+ " 원")
+
+imshow(final_list,final_list[0][0][0][0][5])
+
+# output: [P,S,T,F, sum of single_scores of combination]
 
 #4. Combination Evaluation
+
+
 
 #4.1 DL MODEL
 ##4.1.1 narrow down DB for RI model(난수 and singlescore 내림차순)
--- Algorithm/single_rec.py
+++ Algorithm/single_rec.py
@@ -1,79 +1,234 @@
 from operator import itemgetter
+import numpy as np
 import pymysql
 import random
 
-# 어울림 계수는 1점 만점으로
+color_table = [[85,	65,	65,		55,55,	55],[65, 85,65,	65,	75,	75],[65,65,	85,	75,	65,	65],[55,75,	55,	95,	95,	65],[55,55,	75,	95,	95,	65],[65,65,	65,	75,	75,	95]]
 
 def single_comb(preference,stage,comb_list=[]):
     prime_sort_order = [0,2,4,6,1,5,3,9,7,8]
-    second_sort_order = []
-
+    second_sort_order = [0,1,4,3,2,5,7,6]
+    third_sort_order = [1,3,0,2,4]
+    final_sort_order = [0,1]
 
     # import prime가구 category dB from mysql
     conn, cur = None, None
-    conn = pymysql.connect(host='127.0.0.1',user='root',password='kimjin12',db='furniture_ikea',charset='utf8')
-    cur = conn.cursor
+    conn = pymysql.connect(host='127.0.0.1',user='root',password='kimjin12',db='furniture_DB',charset='utf8')
+    cur = conn.cursor(pymysql.cursors.DictCursor)
+    cur.execute("SELECT Rating, Reviews, Category_Num, Color_2, Color_Num, New_ID, Image_URL, Price FROM furniture_DB.furniture_db  WHERE Category_Num = {}".format(str(preference['furniture_combination'][stage-1])))
+    mysql_list = cur.fetchall()
+    
+    # Create empty list
+    return_list, return_list2, return_list3 = [],[],[]
+    furn_list = []
 
-    cur.execute("SELECT * FROM furniture_detail WHERE furniture_category1 = '{}'".format(str(preference['furniture_combination'][stage])))
-
-    furn_list =[]
-    return_list =[]
-    count = 0
-    random.shuffle(furn_list)
-
+    # Stage 1: Prime Furniture
     if stage == 1:
+        if len(mysql_list) >= 500:
+            furn_list = random.sample(mysql_list,500)
+        else:
+            furn_list = random.sample(mysql_list,int(len(mysql_list)/1.5))
+
         #Single REC 돌리기
         for i,product in enumerate(furn_list):
-            count += 1
-            score = single_rec(product,preference)
+            score = single_rec(product,preference,stage)
             furn_list[i]['single score'] = score
-            if count == 500:
-                break
         
-        #조합 생성
+        #furn_list를 single score의 내림차순 정렬 및 재정렬-> 조합 생성
         sorted_dict = sorted(furn_list, key=itemgetter('single score'), reverse=True)
         for j in prime_sort_order:        
             return_list.append(sorted_dict[j])
 
         return return_list
-
+    
+    # Stage 2: Secnd Furuniture
     elif stage == 2:
-    #Single REC 돌리기
-    #[p1,p2,p3...]
-        second_list = []
-        for k,prime in enumerate(comb_list):
+        if len(mysql_list) >= 150:
+            furn_list = random.sample(mysql_list,150)
+        else:
+            furn_list = random.sample(mysql_list,int(len(mysql_list)/2))
+
+        for prime in comb_list:
+            second_list = []
+            prime_copy = []
+
             for i,product in enumerate(furn_list):
-                count += 1
                 score = single_rec(product,preference,prime)
                 furn_list[i]['single score'] = score
-                if count == 150:
-                    break
+
             #조합 생성
             sorted_dict = sorted(furn_list, key=itemgetter('single score'), reverse=True)
             for j in second_sort_order:        
                 second_list.append(sorted_dict[j])
+                prime_copy.append(prime)
+            
+            combination = [[i,j] for i,j in zip(prime_copy,second_list)]
+
+            return_list.append(combination)
 
         return return_list
-
+    
+    # Stage 3: Third Furniture
     elif stage == 3:
-        return 0
+        if len(mysql_list) >= 150:
+            furn_list = random.sample(mysql_list,150)
+        else:
+            furn_list = random.sample(mysql_list,int(len(mysql_list)/1.3))
 
+        #comb_list = [[p1,s1],[p1,s2]...],[[p2,s1]...]]
+        for prime_second in comb_list:
+            for data in prime_second:
+                prime = data[0]
+                second = data[1]
+                
+                third_list = []
+                prime_copy = []
+                second_copy = []
+
+                for i,product in enumerate(furn_list):
+                    score = single_rec(product,preference,prime,second)
+                    furn_list[i]['single score'] = score
+
+                sorted_dict = sorted(furn_list, key=itemgetter('single score'), reverse=True)
+                for j in third_sort_order:        
+                    third_list.append(sorted_dict[j])
+                    prime_copy.append(prime)
+                    second_copy.append(second)
+                
+                combination = [[i,j,k] for i,j,k in zip(prime_copy,second_copy,third_list)]
+
+                return_list2.append(combination)
+            return_list.append(return_list2)
+            return_list2 = []
+
+        return return_list
+    
+    # Stage 4: Final Furniture
     elif stage == 4:
-        return 0
+        if len(mysql_list) >= 150:
+            furn_list = random.sample(mysql_list,150)
+        else:
+            furn_list = random.sample(mysql_list,int(len(mysql_list)/1.3))
 
+        for prime_second_third in comb_list:
+            for prime_second in prime_second_third:
+                for data in prime_second:
+                    prime = data[0]
+                    second = data[1]
+                    third = data[2]
 
-def single_rec(furn,preference,prime={},second={},third={}):
-    abs = 0
-    rel = 0
+                    final_list = []
+                    prime_copy = []
+                    second_copy = []
+                    third_copy = []
+
+                    for i,product in enumerate(furn_list):
+                        score = single_rec(product,preference,prime,second,third)
+                        furn_list[i]['single score'] = score
+
+                    sorted_dict = sorted(furn_list, key=itemgetter('single score'), reverse=True)
+                    for j in final_sort_order:        
+                        final_list.append(sorted_dict[j])
+                        prime_copy.append(prime)
+                        second_copy.append(second)
+                        third_copy.append(third)
+                    
+                    combination = [[i,j,k,l] for i,j,k,l in zip(prime_copy,second_copy,third_copy,final_list)]
+
+                    return_list3.append(combination)
+                return_list2.append(return_list3)
+                return_list3 = []
+            return_list.append(return_list2)
+            return_list2 = []
+        return return_list
+
+def single_rec(furn,preference,stage,prime={},second={},third={},):
+    abs, rel = 0, 0
+
+    ### Absolute Score
+    review_score, rating_score = 0, 0
+    #Rating Score
+    rating_num = furn['Rating']
+    if rating_num == 'No Review':
+        rating_score = 6
+    else:
+        rating_num = float(rating_num)
+        if rating_num >= 4:
+            rating_score = 10
+        elif rating_num >= 3 and rating_num < 4:
+            rating_score = 7
+        elif rating_num >= 0 and rating_num < 3:
+            rating_score = 3
+        else:
+            rating_score = 6
+
+    #Review Score
+    review_num = furn['Reviews']
+    if review_num == 'No Review':
+        review_score = 7
+    else:
+        review_num = int(review_num)
+        if review_num >= 31:
+            review_score = 10
+        elif review_num >= 11 and review_num < 31:
+            review_score = 9
+        elif review_num >= 6 and review_num < 11:
+            review_score = 8
+        elif review_num >= 0 and review_num < 6:
+            review_score = 7
+        else:
+            review_score = 7
+
+    # Sum of Abs Score
+    abs = (review_score + rating_score + random.uniform(-2, 2)) / 100 
+    rel1, rel2, rel3, rel4 = 0, 0, 0, 0
+
+    ### Relational Score
+    user_color = preference['user_color']
+    furn_color = furn['Color_Num'] - 1
+    user_color_num = len(user_color)
+
+    #Case 1. Prime
+    if stage == 1:
+        for i in user_color:
+            rel1 += color_table[furn_color][i] 
+
+        rel1 = (rel1 / user_color_num)
+        rel = (rel1+ random.uniform(-8, 8)) * 0.8 * 0.01
     
-    #id를 기준으로 생각하자..
+    #Case 2. Second
+    elif stage == 2:
+        for i in user_color:
+            rel1 += color_table[furn_color][i] 
 
+        rel1 = (rel1 / user_color_num)
+        rel2 = color_table[furn_color][prime['Color_Num']-1]
+        rel = ((0.5 * rel1)+(0.5 * rel2)+random.uniform(-7, 7))* 0.8 * 0.01
     
+    #Case 3. Third
+    elif stage == 3:
+        for i in user_color:
+            rel1 += color_table[furn_color][i] 
 
-    #절대적인 가구 추천 계수
-    # rating 
-    # number of reviews는 linear하게 적용하자
+        rel1 = (rel1 / user_color_num)
+        rel2 = color_table[furn_color][prime['Color_Num']-1]
+        rel3 = color_table[furn_color][second['Color_Num']-1]
 
-    score = abs + rel
+        rel = ((0.5 * rel1)+(0.3 * rel2)+(0.2 * rel3)+random.uniform(-8, 8))* 0.8 * 0.01
+    
+    #Case 3. Final    
+    elif stage == 4:
+        for i in user_color:
+            rel1 += color_table[furn_color][i] 
 
-    return score, abs, rel, 
+        rel1 = (rel1 / user_color_num)
+        rel2 = color_table[furn_color][prime['Color_Num']-1]
+        rel3 = color_table[furn_color][second['Color_Num']-1]
+        rel4 = color_table[furn_color][third['Color_Num']-1]
+
+        rel = ((0.3 * rel1)+(0.3 * rel2)+(0.2 * rel3)+(0.2 * rel4)+random.uniform(-6, 6))* 0.8 * 0.01
+
+    # Sum of abs and rel plus random number
+    score = abs + rel + random.uniform(-0.03, 0)
+
+    return score
